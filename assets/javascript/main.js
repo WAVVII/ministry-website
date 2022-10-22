67f0a35d41a8c542cf612760015db48024040dd1
WAVVII <tobiade2012@gmail.com>
2022-10-22 21:11:56 +0100

    jisd

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector('.hamburger');
 const hamburger_icon = hamburger.querySelector('span');
 const mobile_menu = document.querySelector('.mobile-menu');
+const close = document.querySelector('closeme')
 
 hamburger.addEventListener('click', () => {
     hamburger_icon.innerText = hamburger_icon.innerText === 'menu'
