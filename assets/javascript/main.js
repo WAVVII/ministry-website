2b1f78fddf2b906f0215386ec4b01da0e881fc24
WAVVII <tobiade2012@gmail.com>
2022-11-26 21:20:47 +0100

    reset

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,7 +2,6 @@ const hamburger = document.querySelector('.hamburger');
 const hamburger_icon = hamburger.querySelector('span');
 const mobile_menu = document.querySelector('.mobile-menu');
 const close = document.querySelector('closeme')
-const loader = document.querySelector('.loader');
 
 hamburger.addEventListener('click', () => {
     hamburger_icon.innerText = hamburger_icon.innerText === 'menu'
@@ -25,19 +24,4 @@ $(window).scroll(function () {
 btn.on("click", function (e) {
     e.preventDefault();
     $("html, body").animate({ scrollTop: 0 }, "300");
-});
-
-
-window.addEventListener('load', () => {
-
-    // Making 5s delay in website loading
-    setTimeout(() => {
-
-        loader.classList.add('fadeOut');
-        setTimeout(() => {
-            loader.style.display = 'none';
-        }, 1000);
-
-    }, 5000);
-
 });
