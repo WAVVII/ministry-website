fbc7bbcc861e8e250e3e2b1e435c96ea60cf9675
WAVVII <tobiade2012@gmail.com>
2022-12-10 20:31:37 +0100

    preloader

--- assets/javascript/mainanim.js
+++ assets/javascript/mainanim.js
@@ -13,3 +13,11 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.slide');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+// Preloader Code
+
+window.addEventListener("DOMContentLoaded", function() {
+  document.getElementById("preloader").style.display = "none";
+});
+
