110e852cf7f69dc922e80d789ba2ce5ab93d23e2
WAVVII <tobiade2012@gmail.com>
2023-01-09 00:45:04 +0100

    done

--- assets/javascript/mainanim.js
+++ assets/javascript/mainanim.js
@@ -13,21 +13,14 @@ fadeInElements.forEach((element) => {
   observer.observe(element);
 });
 
-var options = {
-  accessibility: true,
-  prevNextButtons: true,
-  pageDots: true,
-  setGallerySize: false
-};
+// Nav Bar scroll color change
 
-var carousel = document.querySelector('[data-carousel]');
-var slides = document.getElementsByClassName('carousel-cell');
-var flkty = new Flickity(carousel, options);
-
-flkty.on('scroll', function () {
-  flkty.slides.forEach(function (slide, i) {
-    var image = slides[i];
-    var x = (slide.target + flkty.x) * -1/3;
-    image.style.backgroundPosition = x + 'px';
-  });
+$(window).scroll(function(){
+  if($(window).scrollTop()){
+    $("header").addClass("white");
+  }
+  else{
+    $("header").removeClass("white")
+  }
 });
+
